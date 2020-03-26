ec07743c22b00b7e7a9d728f6f43e5f3c709c7a3
Vinícius Lourenço <vinyygamerlol@gmail.com>
2020-03-26 08:44:41 -0300

    feat(header): extract header from lists to a component

---
+++ frontend-react/src/components/Header/header.props.ts
@@ -0,0 +1,16 @@
+/**
+ * As propriedades que o componente Header pode receber
+ */
+export interface HeaderProps {
+
+  /**
+   * O texto que ficará no botão de ação
+   */
+  actionButtonText: string;
+
+  /**
+   * A rota para qual será redirecionado o usuário
+   */
+  actionRoute: string;
+
+}
